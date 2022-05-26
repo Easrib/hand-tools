9996b873761688bf6e30e53b0851cd1b011aa87d
Easrib <easrib.sharik01@gmail.com>
2022-05-26 17:26:35 +0600

    image upload to imgbb by post

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,42 +1,59 @@
 import React from 'react';
 import auth from '../../firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 
 const AddReview = () => {
     const [user] = useAuthState(auth);
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
-    const imageStorageKey = '49c7b2acd53a2afd47bb3d25eda3f33f';
-
-    const handleReview = async data => {
+    const imageStorageKey = '29c463e9cd48c287cdd0fe303fb95c81';
+    const onSubmit = async data => {
         const image = data.image[0];
-        const url = `https://api.imgbb.com/1/upload?expiration=600&key=YOUR_CLIENT_API_KEY`
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`
+        fetch(url, {
+            method: 'POST',
+            body: formData
 
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.success) {
+                    const img = result.data.url;
+                    const review = {
+                        name: data.name,
+                        feedback: data.feedback,
+                        img: img,
+                        ratings: data.ratings,
+                        town: data.town
+                    }
+                }
+            })
     }
 
-    const handleRefresh = event => {
-        event.preventDefault();
-    }
     return (
         <div>
             <h2 className='text-2xl text-primary text-center my-3'>Add Review</h2>
-            <form onSubmit={handleReview} onChange={handleRefresh} className='grid grid-cols-1 gap-2 mt-2 justify-items-center'>
+            <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-2 mt-2 justify-items-center'>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span className="label-text text-lg">Your name</span>
                     </label>
-                    <input type="text" value={user.displayName} class="input input-bordered w-full max-w-xs" disabled />
+                    <input {...register("name")} type="text" value={user.displayName} class="input input-bordered w-full max-w-xs" disabled />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span className="label-text text-lg">Your Review</span>
                     </label>
-                    <input type="text" placeholder='Write your review' name='feedback' class="input input-bordered w-full max-w-xs" />
+                    <input {...register("feedback")} type="text" placeholder='Write your review' name='feedback' class="input input-bordered w-full max-w-xs" />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span className="label-text text-lg">Pick your ratings</span>
                     </label>
-                    <select class="select select-bordered">
+                    <select {...register("ratings")} class="select select-bordered">
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -48,13 +65,13 @@ const AddReview = () => {
                     <label class="label">
                         <span className="label-text text-lg">Your Location</span>
                     </label>
-                    <input type="text" name='town' placeholder='Write your location' class="input input-bordered w-full max-w-xs" />
+                    <input {...register("town")} type="text" name='town' placeholder='Write your location' class="input input-bordered w-full max-w-xs" />
                 </div>
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                         <span className="label-text text-lg">Upload your Image</span>
                     </label>
-                    <input type="file" name='phone' placeholder='Write your phone number' class="input input-bordered w-full max-w-xs" />
+                    <input {...register("image")} type="file" placeholder='Write your phone number' class="input input-bordered w-full max-w-xs" />
                 </div>
                 <input className='btn w-full max-w-xs' value="Submit" type="submit" />
             </form>
